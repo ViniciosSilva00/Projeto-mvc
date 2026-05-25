00d0e5df5c22d5e537e67fa626c64cb22c1199c1
vinicios <viniciosbezerra06@gmail.com>
2026-05-25 10:44:37 -0300

    tabelas categorias e produtos feitas

---
+++ app/models/__init__.py
@@ -0,0 +1,8 @@
+from app.models import categoria
+from app.models import produto
+from app.models import usuario
+
+
+
+# python -m alembic revision --autogenerate -m "criar tabelas categorias e produtos"
+# python -m alembic upgrade head
---
+++ app/models/produto.py
@@ -0,0 +1,30 @@
+# Tabela de protudos
+
+from sqlalchemy import Column, Integer, String, Float, Boolean, ForeignKey
+from sqlalchemy.orm import relationship
+from app.database import Base
+
+class Produto(Base):
+    __tablename__ = "produtos"
+
+    id = Column(Integer, primary_key=True, autoincrement=True, index=True)
+    nome = Column(String(200), nullable=False, index=True)
+    preco = Column(Float, nullable=False, default=0.0)
+    estoque_atual = Column(Integer, nullable=False, default=0)
+    ativo = Column(Boolean, default=True)
+
+    imagem_path = Column(String(250), nullable=True)
+
+    categoria_id = Column(Integer, ForeignKey("categorias.id"),onupdate="SET NULL", nullable=True)
+
+    categoria = relationship("Categoria", back_populates="produtos")
+
+    #método
+    @property
+    def imagem_url(self):
+        if self.imagem_path:
+            return f"/static/{self.imagem_path}"
+        else:
+            return "/static/img/produto-placeholder.png"
+        
+    produto.ima
